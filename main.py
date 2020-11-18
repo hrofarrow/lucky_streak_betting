fc9f80fa13f3bc6758543026828515d860e9cce9
yaggij <jyaggi55@gmail.com>
2020-11-17 20:57:56 -0500

    added py files and updated pandas file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from model_files.ml_model import model
 
 # creating a flask app and naming it "app"
 app = Flask('app')
@@ -11,3 +12,18 @@ def test():
 
 if __name__ == "__main__":
     app.run(debug=True, port=9696)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    vehicle = request.get_json()
+    print(vehicle)
+    with open('./model_files/model.bin', 'rb') as f_in:
+        model = pickle.load(f_in)
+        f_in.close()
+    predictions = predict_mpg(vehicle, model)
+
+    result = {
+        'mpg_prediction': list(predictions)
+    }
+    return jsonify(result)
---
+++ model_files/ml_model.py
@@ -0,0 +1,100 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+import numpy as np
+
+stats = pd.read_csv(os.path.join("csv/MLB DB V1 CSV.csv"))
+stats.rename(columns={"O/U Odds": "O/U_Odds"}, inplace= True)
+
+stats.FIELD[stats.FIELD == 'HOME'] = 1
+stats.FIELD[stats.FIELD == 'AWAY'] = 0
+
+stats = stats.drop(columns = ["Team", "Season", "Date", "Vs", "Score", "Away Starter", "Home Starter", "BAL Line", "O/U", "O/U Result", "Unnamed: 9", "Margin", "Runs Scr", "Runs Alw"])
+
+stats= stats.dropna()
+
+stats.head()
+
+stats.drop(stats[stats["O/U_Odds"] == "9 -"].index, inplace=True)
+stats.drop(stats[stats["O/U_Odds"] == "7 -"].index, inplace=True)
+stats.drop(stats[stats["O/U_Odds"] == "8 -"].index, inplace=True)
+stats.drop(stats[stats["O/U_Odds"] == "10 -"].index, inplace=True)
+stats.drop(stats[stats["O/U_Odds"] == "11 -"].index, inplace=True)
+stats.drop(stats[stats["O/U_Odds"] == "6 -"].index, inplace=True)
+
+stats.FIELD[stats.FIELD == 'HOME'] = 1
+stats.FIELD[stats.FIELD == 'AWAY'] = 0
+
+# Assign X (data) and y (target)
+X = stats.drop("Game Result", axis=1)
+y = stats["Game Result"]
+print(X.shape, y.shape)
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from tensorflow.keras.utils import to_categorical
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+X_scaler = MinMaxScaler().fit(X_train)
+X_train_scaled = X_scaler.transform(X_train)
+X_test_scaled = X_scaler.transform(X_test)
+
+
+# Step 1: Label-encode data set
+label_encoder = LabelEncoder()
+label_encoder.fit(y_train)
+encoded_y_train = label_encoder.transform(y_train)
+encoded_y_test = label_encoder.transform(y_test)
+
+# Step 2: Convert encoded labels to one-hot-encoding
+y_train_categorical = to_categorical(encoded_y_train)
+y_test_categorical = to_categorical(encoded_y_test)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression()
+classifier
+
+classifier.fit(X_train, y_train)
+
+predictions = classifier.predict(X_test)
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(units=100, activation='relu', input_dim=4))
+model.add(Dense(units=100, activation='relu'))
+model.add(Dense(units=2, activation='softmax'))
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+model.fit(
+    X_train_scaled,
+    y_train_categorical,
+    epochs=60,
+    shuffle=True,
+    verbose=0
+)
+
+
+model_loss, model_accuracy = model.evaluate(
+    X_test_scaled, y_test_categorical, verbose=2)
+print(
+    f"Normal Neural Network - Loss: {model_loss}, Accuracy: {model_accuracy}")
